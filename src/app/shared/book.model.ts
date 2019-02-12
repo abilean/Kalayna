d252cc8b3a94a80fe62732ec3bf5bfd66feda1cf
abilean <abilean@gmail.com>
2019-02-11 20:43:45 -0500

    added store links

--- src/app/shared/book.model.ts
+++ src/app/shared/book.model.ts
@@ -9,10 +9,21 @@ export interface IBook {
   series: BookSeries;
   imagePath: string;
   description: string[];
+  genre: string;
+  isbn: string;
+  publisher: string;
+  stores: IMerchant[];
+  audio: IMerchant[];
+  foreign: IMerchant[];
   firstChapter: string[];
 }
 
 export interface IChapter {
   name: string;
   firstChapter: string[];
+}
+
+export interface IMerchant {
+  name: string;
+  link: string;
 }
